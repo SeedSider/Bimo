73e5fc93b79869369afdfaea9961240784262182
Usman Sidiq <usman.sidiq@ocbcnisp.com>
2023-11-03 22:31:52 +0700

    haha

---
+++ app/src/main/java/com/kentang/testnotif/astray/MainActivity.kt
@@ -0,0 +1,34 @@
+package com.kentang.testnotif.astray
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
+import com.google.android.gms.tasks.OnCompleteListener
+import com.google.firebase.messaging.FirebaseMessaging
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        FirebaseMessaging.getInstance().token.addOnCompleteListener(OnCompleteListener { task ->
+            if (!task.isSuccessful) {
+                Log.d(TAG, "Fetching FCM registration token failed", task.exception)
+                return@OnCompleteListener
+            }
+
+            // Get new FCM registration token
+            val token = task.result
+
+            // Log and toast
+            Log.d(TAG, token)
+            Toast.makeText(baseContext, token, Toast.LENGTH_SHORT).show()
+        })
+    }
+
+    companion object {
+        private const val TAG = "MainActivityCoy"
+    }
+}
